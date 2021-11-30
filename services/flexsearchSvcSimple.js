79fcf8d73d971e5183670a7cf10c202d4355ff98
smori <shinichiro.mori.1983@gmail.com>
2021-11-30 23:03:50 +0900

    Split code to misc/cjk-tokenizer.js

--- services/flexsearchSvcSimple.js
+++ services/flexsearchSvcSimple.js
@@ -3,11 +3,11 @@ import Flexsearch from 'flexsearch'
 // import cjkCharset from 'flexsearch/dist/lang/cjk/default.min.js'
 import _ from 'lodash'
 
+import cjkTokenizer from '../misc/cjk-tokenizer'
+
 let index = null
 let cjkIndex = null
 let pagesByPath = null
-
-const cjkRegex = /[\u3131-\u314e|\u314f-\u3163|\uac00-\ud7a3]|[\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d]|[\u3041-\u3096]|[\u30A1-\u30FA]/giu
 
 export default {
   buildIndex(allPages, options) {
@@ -34,17 +34,7 @@ export default {
       cjkIndex = new Flexsearch({
         ...indexSettings,
         encode: false,
-        tokenize: function(str) {
-          const cjkWords = []
-          let m = null
-          do {
-            m = cjkRegex.exec(str)
-            if (m) {
-              cjkWords.push(m[0])
-            }
-          } while (m)
-          return cjkWords
-        },
+        tokenize: cjkTokenizer,
       })
       cjkIndex.add(cjkPages)
     }
